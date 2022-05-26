5ec51b69426616d6c3840b9b3097b3a015b90b3d
Jack Windham <jckwind11@vt.edu>
2022-05-26 19:43:31 -0400

    finished

--- spam-c/src/app/app.module.ts
+++ spam-c/src/app/app.module.ts
@@ -9,12 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material-module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { SkeletonModule } from 'primeng/skeleton';
-import { TableModule } from 'primeng/table';
-import { CaptchaModule } from 'primeng/captcha';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { InputTextModule } from 'primeng/inputtext';
 import { SettingsComponent } from './settings/settings.component';
 
@@ -31,11 +25,6 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    SkeletonModule,
-    TableModule,
-    CaptchaModule,
-    AccordionModule,
-    ProgressSpinnerModule,
     InputTextModule,
     FormsModule,
     ReactiveFormsModule,
